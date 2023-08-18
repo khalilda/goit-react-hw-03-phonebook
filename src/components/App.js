33fd637f92a0ba9879506d19af4c4bfa08d815c2
khalilda <100872154+khalilda@users.noreply.github.com>
2023-08-18 15:24:13 +0300

    hw 3

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ export class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Ihor Kalyta', number: '577-99-77' },
+      { id: 'id-5', name: 'Khalil Dajani', number: '577-99-77' },
     ],
     filter: '',
   };
